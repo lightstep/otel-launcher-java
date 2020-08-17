b0ec2b44c35688169fe2d9ecc421c98986595263
Sergei Malafeev <sergei@malafeev.org>
2020-08-17 20:15:35 +0800

    merge manual and auto examples into one
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

--- examples/agent/src/main/java/com/lightstep/opentelemetry/launcher/example/App.java
+++ examples/agent/src/main/java/com/lightstep/opentelemetry/launcher/example/App.java
@@ -1,12 +1,19 @@
 package com.lightstep.opentelemetry.launcher.example;
 
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.extensions.auto.annotations.WithSpan;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Tracer;
 import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class App {
-  public static void main(String[] args) throws Exception {
+
+  @WithSpan
+  public static void nestedMethod() throws Exception {
     OkHttpClient client = new OkHttpClient();
     Request request = new Request.Builder()
         .url("https://www.google.com")
@@ -18,6 +25,19 @@ public class App {
 
     client.dispatcher().executorService().shutdown();
     client.connectionPool().evictAll();
+  }
+
+  public static void firstMethod() throws Exception {
+    Tracer tracer = OpenTelemetry.getTracer(System.getenv("LS_SERVICE_NAME"));
+    Span span = tracer.spanBuilder("firstMethod").startSpan();
+    try (Scope scope = tracer.withSpan(span)) {
+      nestedMethod();
+    }
+    span.end();
+  }
+
+  public static void main(String[] args) throws Exception {
+    firstMethod();
 
     TimeUnit.SECONDS.sleep(30);
   }
