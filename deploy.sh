a139afc6d063e6e7c761e5bf486cfdabf97d3da4
Sergei Malafeev <sergei@malafeev.org>
2021-01-06 20:15:19 +0800

    print deploy steps
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,11 @@ VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpress
 
 echo "Publishing $VERSION"
 
-# Build and deploy to Bintray
-# (do not include the agent jar).
+echo "Build and deploy to Bintray (do not include the agent jar)"
 mvn -s .circleci.settings.xml -Dmaven.test.skip=true deploy -pl .,common,launcher
 
-# Sign the jar and other files in Bintray
+echo "Sign the jar and other files in Bintray"
 curl -H "X-GPG-PASSPHRASE:$BINTRAY_GPG_PASSPHRASE" -u $BINTRAY_USER:$BINTRAY_API_KEY -X POST "https://api.bintray.com/gpg/lightstep/maven/otel-launcher-java/versions/$VERSION"
 
-# Sync the repository with Maven Central
+echo "Sync the repository with Maven Central"
 curl -H "Content-Type: application/json" -u $BINTRAY_USER:$BINTRAY_API_KEY -X POST -d '{"username":"'$MAVEN_CENTRAL_USER_TOKEN'","password":"'$MAVEN_CENTRAL_TOKEN_PASSWORD'","close":"1"}' "https://api.bintray.com/maven_central_sync/lightstep/maven/otel-launcher-java/versions/$VERSION"
