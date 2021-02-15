ebaf9ee9ccbf081e8d2447e578f26e0932ba5461
Jingguo Yao <yaojingguo@gmail.com>
2021-02-15 11:31:21 +0800

    Use Span.Kind instead of SpanKind

--- examples/launcher/src/main/java/com/lightstep/opentelemetry/launcher/example/Main.java
+++ examples/launcher/src/main/java/com/lightstep/opentelemetry/launcher/example/Main.java
@@ -3,7 +3,6 @@ package com.lightstep.opentelemetry.launcher.example;
 import com.lightstep.opentelemetry.launcher.OpenTelemetryConfiguration;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,7 +20,7 @@ public class Main {
         .buildOpenTelemetry();
 
     Tracer tracer = openTelemetry.getTracer("LightstepExample");
-    Span span = tracer.spanBuilder("start example").setSpanKind(SpanKind.CLIENT).startSpan();
+    Span span = tracer.spanBuilder("start example").setSpanKind(Span.Kind.CLIENT).startSpan();
     span.setAttribute("Attribute 1", "Value 1");
     span.addEvent("Event 0");
     // execute my use case - here we simulate a wait
