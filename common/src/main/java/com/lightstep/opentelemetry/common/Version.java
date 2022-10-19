ecb438b2fdf1e1d8b47b3b0279658b684313bd51
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2022-10-19 17:17:23 +0200

    VERSION 1.19.0

--- common/src/main/java/com/lightstep/opentelemetry/common/Version.java
+++ common/src/main/java/com/lightstep/opentelemetry/common/Version.java
@@ -2,7 +2,7 @@
 package com.lightstep.opentelemetry.common;
 
 class Version {
-    private static final String LAUNCHER_VERSION = "1.17.0";
+    private static final String LAUNCHER_VERSION = "1.19.0";
 
     static String get() {
       return LAUNCHER_VERSION;
